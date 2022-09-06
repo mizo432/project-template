d86437969e1ce78d32b47a46c7d1f5764e79b86e
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2022-09-07 02:36:36 +0900

    Configにアノテーションを定義した

--- web-template/src/main/java/org/venusPj/projectTemplate/WebTemplateApplication.java
+++ web-template/src/main/java/org/venusPj/projectTemplate/WebTemplateApplication.java
@@ -1,13 +1,10 @@
-package org.venusPj;
+package org.venusPj.projectTemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
-@EnableCaching
 public class WebTemplateApplication {
 
     public static void main(String[] args) {
---
+++ web-template/src/main/java/org/venusPj/projectTemplate/web/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * プレゼンテーション層web用のコントローラーをパッケージを作成し格納する
+ */
+package org.venusPj.projectTemplate.web;
+
