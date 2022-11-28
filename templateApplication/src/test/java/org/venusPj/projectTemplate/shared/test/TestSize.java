2a7bcc7003598f0e781eecb8c2586370c3449bd7
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2022-11-29 02:27:02 +0900

    feat ( TestSize ):add TestSize class.

---
+++ templateApplication/src/test/java/org/venusPj/projectTemplate/shared/test/TestSize.java
@@ -0,0 +1,9 @@
+package org.venusPj.projectTemplate.shared.test;
+
+public class TestSize {
+
+    public final static String SMALL = "small";
+    public final static String MEDIUM = "medium";
+    public final static String LARGE = "large";
+
+}
