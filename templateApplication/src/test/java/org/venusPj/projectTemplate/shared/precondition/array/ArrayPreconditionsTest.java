015f222faad9bd83c8759d2ab0b9b27a8f96eb5d
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2022-11-29 02:32:18 +0900

    feat ( ArrayPreconditions ):change ArrayPreconditions class

--- templateApplication/src/test/java/org/venusPj/projectTemplate/shared/precondition/array/ArrayPreconditionsTest.java
+++ templateApplication/src/test/java/org/venusPj/projectTemplate/shared/precondition/array/ArrayPreconditionsTest.java
@@ -2,8 +2,13 @@ package org.venusPj.projectTemplate.shared.precondition.array;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.google.common.collect.Range;
+import java.util.function.Supplier;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.venusPj.projectTemplate.shared.test.TestSize;
 
+@Tag(TestSize.SMALL)
 class ArrayPreconditionsTest {
 
     /**
@@ -31,6 +36,92 @@ class ArrayPreconditionsTest {
         assertThrows(IllegalArgumentException.class,
             () -> ArrayPreconditions.checkNotEmpty(new Object[]{}));
     }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkAllItemPresent(Object[])}
+     */
+    @Test
+    void testCheckAllItemPresent() {
+        // Arrange
+        Object[] referenceArray = null;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkAllItemPresent(referenceArray));
+    }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkAllItemPresent(Object[])}
+     */
+    @Test
+    void testCheckAllItemPresent2() {
+        // Arrange
+        Object object = null;
+        Object[] referenceArray = new Object[]{object};
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkAllItemPresent(referenceArray));
+    }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkAllItemPresent(Object[], Supplier)}
+     */
+    @Test
+    void testCheckAllItemPresent3() {
+        // Arrange
+        Object[] referenceArray = null;
+        Supplier<RuntimeException> runtimeExceptionSupplier = null;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkAllItemPresent(referenceArray, runtimeExceptionSupplier));
+
+    }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkLength(Object[], Range)}
+     */
+    @Test
+    void testCheckLength() {
+        // Arrange
+        String string = "Reference Array";
+        Object[] referenceArray = new Object[]{string};
+        Range<Integer> sizeRange = Range.all();
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkLength(referenceArray, sizeRange));
+    }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkLength(Object[], Range)}
+     */
+    @Test
+    void testCheckLength2() {
+        // Arrange
+        Object[] referenceArray = null;
+        Range<Integer> sizeRange = null;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkLength(referenceArray, sizeRange));
+    }
+
+    /**
+     * Method under test: {@link ArrayPreconditions#checkLength(Object[], Range)}
+     */
+    @Test
+    void testCheckLength3() {
+        // Arrange
+        Object[] referenceArray = null;
+        Range<Integer> sizeRange = Range.all();
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class,
+            () -> ArrayPreconditions.checkLength(referenceArray, sizeRange));
+    }
+
 
 }
 
