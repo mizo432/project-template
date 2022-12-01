99f645c8de25901edd83078a15ecbe4f016c9856
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2022-12-01 22:07:21 +0900

    gitIgnoreファイルを更新した

---
+++ template-web-admin/src/main/java/org/venuspj/template/admin/TemplateWebAdminApplication.java
@@ -0,0 +1,13 @@
+package org.venuspj.template.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateWebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateWebAdminApplication.class, args);
+    }
+
+}
---
+++ template-web-admin/src/test/java/org/venuspj/template/admin/TemplateWebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.venuspj.template.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateWebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
