9ef2c0aa5aab35db125501189ca9d756e5a94953
JAGUNMA\80-k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2022-12-14 16:00:18 +0900

    WebMvcConfigurerを有効にできるようにした

--- templateApplication/src/main/java/org/venusPj/projectTemplate/modules/TemplateMvcConfig.java
+++ templateApplication/src/main/java/org/venusPj/projectTemplate/modules/TemplateMvcConfig.java
@@ -1,23 +1,36 @@
 package org.venusPj.projectTemplate.modules;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.EncodedResourceResolver;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
+@EnableWebMvc
 public class TemplateMvcConfig implements WebMvcConfigurer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TemplateMvcConfig.class);
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-            .addResourceLocations("classpath:/static/")
+        LOG.info("TemplateMvcConfig#addResourceHandlers(ResourceHandlerRegistry)");
+        registry.addResourceHandler("/css/**")
+            .addResourceLocations("classpath:/static/css/")
             .setCachePeriod(604800)
             .resourceChain(true) // プロダクション環境ではtrue/開発環境ではfalseがおすすめ
             .addResolver(new VersionResourceResolver()
-                    .addContentVersionStrategy("/**/**.css")
-                    .addContentVersionStrategy("/**/**.js")
+                    .addContentVersionStrategy("/**/*.css")
+                // コンテンツデータのMD5ハッシュ値によるバージョニング機能の有効化
+            );
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("classpath:/static/images/")
+            .setCachePeriod(604800)
+            .resourceChain(true) // プロダクション環境ではtrue/開発環境ではfalseがおすすめ
+            .addResolver(new VersionResourceResolver()
                     .addContentVersionStrategy("/**/*.png")
                 // コンテンツデータのMD5ハッシュ値によるバージョニング機能の有効化
             );
