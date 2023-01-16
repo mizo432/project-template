d32d48aaf423450fb0a0f9db5b14d3967aeb22be
JAGUNMA\80-k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2023-01-16 16:51:36 +0900

    add template-web-application project.

---
+++ template-web-application/src/main/java/com/undecided/projectTemplate/modules/TemplateWebApplication.java
@@ -0,0 +1,16 @@
+package com.undecided.projectTemplate.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.undecided.projectTemplate")
+public class TemplateWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateWebApplication.class, args);
+    }
+
+
+}
