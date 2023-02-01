d0ef05d1d4240cdc04f3d4e1bdd91b9af01e0ea8
JAGUNMA\80-k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2023-02-01 15:35:46 +0900

    README.mdを更新した

--- server-business/src/test/java/com/undecided/projectTemplate/modules/resource/domain/story/estimate/BusinessTests.java
+++ server-business/src/test/java/com/undecided/projectTemplate/modules/resource/domain/story/estimate/BusinessTests.java
@@ -1,13 +1,13 @@
 package com.undecided.projectTemplate.modules.resource.domain.story.estimate;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class BusinessTests {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessTests.class, args);
     }
 
 }
