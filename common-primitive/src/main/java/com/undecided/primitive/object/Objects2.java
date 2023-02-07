d56110dc326c409cfb96104132882c8d7e4748e2
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2023-02-08 05:40:55 +0900

    backendをスリム化

--- common-primitive/src/main/java/com/undecided/primitive/object/Objects2.java
+++ common-primitive/src/main/java/com/undecided/primitive/object/Objects2.java
@@ -1,16 +1,18 @@
 package com.undecided.primitive.object;
 
+import java.util.Objects;
+
 public class Objects2 {
 
     public static final Object[] EMPTY_ARRAY = new Object[0];
 
-    public static boolean isNull(Object refernce) {
-        return refernce == null;
+    public static boolean isNull(Object reference) {
+        return reference == null;
 
     }
 
     public static boolean equal(Object o, Object o1) {
-        return Objects2.equal(0, 01);
+        return Objects.equals(o, o1);
     }
 
     public static boolean nonNull(Object o) {
