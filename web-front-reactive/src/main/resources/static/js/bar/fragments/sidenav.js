009f4b1264f6bc62368f518f387f6897e6013d03
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-07-18 06:29:23 +0900

    #1 layoutを使用してみる

---
+++ web-front-reactive/src/main/resources/static/js/bar/fragments/sidenav.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, {
+// specify options here
+    });
+});
