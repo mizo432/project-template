98d4db834acfac4c59a4735b391f54bb25fbaac6
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-08-09 23:28:07 +0900

    project-template-7 CRUD機能を作成する
    POSTの通信が飛ばないところまで確認した

--- web-front-reactive/src/main/java/com/undecided/template/modules/foo/models/foo/admin/employees/EmployeesRepository.java
+++ web-front-reactive/src/main/java/com/undecided/template/modules/foo/models/foo/admin/employees/EmployeesRepository.java
@@ -1,7 +1,9 @@
 package com.undecided.template.modules.foo.models.foo.admin.employees;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeesRepository extends R2dbcRepository<Employee, Long> {
 
 }
