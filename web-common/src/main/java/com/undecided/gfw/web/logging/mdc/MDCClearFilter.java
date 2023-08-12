e37cb3a42965dc708ed644787ba6f1e018050a34
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-08-13 06:17:11 +0900

    #17 画面デザインをフラグメントにまとめる
    静的コンテンツが404を解消

--- web-common/src/main/java/com/undecided/gfw/web/logging/mdc/MDCClearFilter.java
+++ web-common/src/main/java/com/undecided/gfw/web/logging/mdc/MDCClearFilter.java
@@ -4,16 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 public class MDCClearFilter extends OncePerRequestFilter {
 
     @Override
-    protected final void doFilterInternal(HttpServletRequest request,
-        HttpServletResponse response,
-        FilterChain filterChain) throws ServletException, IOException {
+    protected final void doFilterInternal(@NotNull HttpServletRequest request,
+                                          @NotNull HttpServletResponse response,
+                                          FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
         } finally {
--- web-common/src/main/java/com/undecided/gfw/web/logging/mdc/XTrackMDCPutFilter.java
+++ web-common/src/main/java/com/undecided/gfw/web/logging/mdc/XTrackMDCPutFilter.java
@@ -2,6 +2,7 @@ package com.undecided.gfw.web.logging.mdc;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.util.UUID;
 import java.util.regex.Pattern;
 
@@ -13,11 +14,12 @@ public class XTrackMDCPutFilter extends AbstractMDCPutFilter {
 
     public void setAttributeName(String attributeName) {
         this.attributeName = attributeName;
+
     }
 
     @Override
     protected String getMDCKey(HttpServletRequest request,
-        HttpServletResponse response) {
+                               HttpServletResponse response) {
         return attributeName;
     }
 
@@ -29,7 +31,7 @@ public class XTrackMDCPutFilter extends AbstractMDCPutFilter {
 
     @Override
     protected String getMDCValue(HttpServletRequest request,
-        HttpServletResponse response) {
+                                 HttpServletResponse response) {
         String xTrack = request.getHeader(attributeName);
         if (xTrack == null) {
             xTrack = createXTrack();
