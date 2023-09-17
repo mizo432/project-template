3d7aa33772a02e7b3cd6445d0a715406051742b6
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-17 14:09:57 +0900

    #69 pmdが動作するようにする

--- server-business/src/main/java/template/modules/admin/domain/model/project/ProjectRepository.java
+++ server-business/src/main/java/template/modules/admin/domain/model/project/ProjectRepository.java
@@ -15,21 +15,56 @@ import java.util.List;
 @Dao
 public interface ProjectRepository {
 
+    /**
+     * プロジェクトIDを元にプロジェクトを検索する
+     *
+     * @param projectId プロジェクトID
+     * @return 検索結果
+     */
     @Select
     Project findOneById(SnowflakeId projectId);
 
+    /**
+     * プロジェクトを全件検索する
+     *
+     * @return 検索結果
+     */
     @Select
     List<Project> selectAll();
 
+    /**
+     * プロジェクトを新規追加する
+     *
+     * @param project プロジェクト
+     * @return 新規登録結果
+     */
     @Insert
     Result<Project> insert(Project project);
 
+    /**
+     * プロジェクトを更新する
+     *
+     * @param project プロジェクト
+     * @return 更新結果
+     */
     @Update
     Result<Project> update(Project project);
 
+    /**
+     * プロジェクトコードを元にプロジェクトを検索する
+     *
+     * @param projectCode プロジェクトコード
+     * @return 検索結果
+     */
     @Select
     Project findOneByProjectCode(ProjectCode projectCode);
 
+    /**
+     * プロジェクトIDを元にプロジェクトを削除する
+     *
+     * @param projectId プロジェクトID
+     * @return 削除件数
+     */
     @Delete(sqlFile = true)
     int deleteById(SnowflakeId projectId);
 }
