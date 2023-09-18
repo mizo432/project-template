85e9146d3f439347ce400d94d6098c44e90e9428
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-18 19:35:20 +0900

    #50 用語を追加する

--- server-business/src/main/java/template/modules/admin/domain/model/project/attribute/StoryCodePrefix.java
+++ server-business/src/main/java/template/modules/admin/domain/model/project/attribute/StoryCodePrefix.java
@@ -1,33 +1,64 @@
 package template.modules.admin.domain.model.project.attribute;
 
-import com.undecided.primitive.string.Strings2;
 import com.undecided.projectTemplate.shared.precondition.string.StringPreconditions;
 import lombok.Getter;
 import org.seasar.doma.Domain;
 import template.shared.value.AbstractStringValue;
 
+/**
+ * ストリーコードプレフィックス
+ */
 @Getter
 @Domain(valueType = String.class, factoryMethod = "reconstruct")
 public class StoryCodePrefix extends AbstractStringValue {
 
-    private static final StoryCodePrefix EMPTY = new StoryCodePrefix(Strings2.EMPTY);
+    private static final StoryCodePrefix EMPTY = new StoryCodePrefix();
 
-    StoryCodePrefix(String value) {
+    /**
+     * コンストラクター
+     *
+     * @param value 値
+     */
+    StoryCodePrefix(final String value) {
         super(value);
     }
 
+    /**
+     * コンストラクター
+     */
+    public StoryCodePrefix() {
+
+    }
+
+    /**
+     * nullオブジェクト取得
+     *
+     * @return ストーリーコードプレフィックス
+     */
     public static StoryCodePrefix empty() {
         return EMPTY;
     }
 
 
-    public static StoryCodePrefix of(String value) {
+    /**
+     * ファクトリーメソッド
+     *
+     * @param value 値
+     * @return ストーリーコードプレフィックス
+     */
+    public static StoryCodePrefix of(final String value) {
         StringPreconditions.checkNotEmpty(value, "ストーリーコードの接頭子");
         return new StoryCodePrefix(value);
 
     }
 
-    public static StoryCodePrefix reconstruct(String value) {
+    /**
+     * 再生成メソッド
+     *
+     * @param value 値
+     * @return ストーリーコードプレフィックス
+     */
+    public static StoryCodePrefix reconstruct(final String value) {
         return new StoryCodePrefix(value);
 
     }
