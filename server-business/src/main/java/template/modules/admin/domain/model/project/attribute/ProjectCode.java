7c8e979d2b5a503d7bf28c2beb4ebfc7644906f4
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-18 19:35:20 +0900

    #50 用語を追加する

--- server-business/src/main/java/template/modules/admin/domain/model/project/attribute/ProjectCode.java
+++ server-business/src/main/java/template/modules/admin/domain/model/project/attribute/ProjectCode.java
@@ -1,6 +1,5 @@
 package template.modules.admin.domain.model.project.attribute;
 
-import com.undecided.primitive.string.Strings2;
 import com.undecided.projectTemplate.shared.precondition.string.StringPreconditions;
 import lombok.Getter;
 import org.seasar.doma.Domain;
@@ -10,10 +9,22 @@ import template.shared.value.AbstractStringValue;
 @Domain(valueType = String.class, factoryMethod = "reconstruct")
 public class ProjectCode extends AbstractStringValue {
 
-    private static final ProjectCode EMPTY = new ProjectCode(Strings2.EMPTY);
+    private static final ProjectCode EMPTY = new ProjectCode();
 
-    ProjectCode(String value) {
+    /**
+     * コンストラクター
+     *
+     * @param value 値
+     */
+    ProjectCode(final String value) {
         super(value);
+    }
+
+    /**
+     * コンストラクター
+     */
+    public ProjectCode() {
+
     }
 
 
@@ -22,13 +33,25 @@ public class ProjectCode extends AbstractStringValue {
     }
 
 
-    public static ProjectCode of(String value) {
+    /**
+     * ファクトリーメソッド
+     *
+     * @param value 値
+     * @return プロジェクトコード
+     */
+    public static ProjectCode of(final String value) {
         StringPreconditions.checkNotEmpty(value, "プロダクトコード");
         return new ProjectCode(value);
 
     }
 
-    public static ProjectCode reconstruct(String value) {
+    /**
+     * 再生成メソッド
+     *
+     * @param value 値
+     * @return プロジェクトコード
+     */
+    public static ProjectCode reconstruct(final String value) {
         return new ProjectCode(value);
 
 
