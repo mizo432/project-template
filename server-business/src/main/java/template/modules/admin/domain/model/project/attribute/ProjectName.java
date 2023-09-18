c061b45ad7c656d62d5c2966685814c2b6102be1
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-18 19:19:18 +0900

    #50 用語を追加する

--- server-business/src/main/java/template/modules/admin/domain/model/project/attribute/ProjectName.java
+++ server-business/src/main/java/template/modules/admin/domain/model/project/attribute/ProjectName.java
@@ -6,29 +6,61 @@ import lombok.Getter;
 import org.seasar.doma.Domain;
 import template.shared.value.AbstractStringValue;
 
+/**
+ * プロジェクト名
+ */
 @Getter
 @Domain(valueType = String.class, factoryMethod = "reconstruct")
 public class ProjectName extends AbstractStringValue {
 
-    private static final ProjectName EMPTY = new ProjectName(Strings2.EMPTY);
+    private static final ProjectName EMPTY = new ProjectName();
 
-    public ProjectName(String value) {
+    /**
+     * コンストラクター
+     *
+     * @param value 値
+     */
+    ProjectName(final String value) {
         super(value);
 
     }
 
+    /**
+     * コンストラクター
+     */
+    public ProjectName() {
 
+    }
+
+
+    /**
+     * ヌルオブジェクトを取得する
+     *
+     * @return プロジェクト名
+     */
     public static ProjectName empty() {
         return EMPTY;
     }
 
-    public static ProjectName of(String value) {
+    /**
+     * ファクトリーメソッド
+     *
+     * @param value 値
+     * @return プロジェクト名
+     */
+    public static ProjectName of(final String value) {
         StringPreconditions.checkNotEmpty(value, "プロダクト名");
         return new ProjectName(value);
 
     }
 
-    public static ProjectName reconstruct(String value) {
+    /**
+     * 再生成
+     *
+     * @param value 値
+     * @return プロおジェクト名
+     */
+    public static ProjectName reconstruct(final String value) {
         if (Strings2.isEmpty(value))
             return EMPTY;
         return new ProjectName(value);
