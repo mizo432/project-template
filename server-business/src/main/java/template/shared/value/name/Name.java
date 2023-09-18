77760a52ebef7eb15473ec87f4269cefafe33ffc
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-19 00:19:52 +0900

    #50 用語を追加する

--- server-business/src/main/java/template/shared/value/name/Name.java
+++ server-business/src/main/java/template/shared/value/name/Name.java
@@ -5,20 +5,42 @@ import lombok.Getter;
 import org.seasar.doma.Domain;
 import template.shared.value.AbstractStringValue;
 
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 名称
+ */
 @Getter
 @Domain(valueType = String.class, factoryMethod = "reconstruct")
-public class Name extends AbstractStringValue {
+public class Name extends AbstractStringValue implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private static final Name EMPTY_VALUE = new Name();
 
-    Name(String value) {
+    /**
+     * コンストラクター
+     *
+     * @param value 値
+     */
+    /* default */Name(String value) {
         super(value);
     }
 
+    /**
+     * コンストラクター
+     */
     public Name() {
         super();
     }
 
+    /**
+     * ファクトリーメソッド
+     *
+     * @param value 値
+     * @return 名称
+     */
     public static Name of(String value) {
         if (Strings2.isEmpty(value)) {
             return EMPTY_VALUE;
@@ -28,6 +50,12 @@ public class Name extends AbstractStringValue {
 
     }
 
+    /**
+     * 再生成メソッド
+     *
+     * @param value 値
+     * @return 名称
+     */
     public static Name reconstruct(String value) {
         if (Strings2.isEmpty(value)) {
             return EMPTY_VALUE;
@@ -36,6 +64,11 @@ public class Name extends AbstractStringValue {
 
     }
 
+    /**
+     * 空オブジェクトを取得する
+     *
+     * @return 名称
+     */
     public static Name empty() {
         return EMPTY_VALUE;
     }
