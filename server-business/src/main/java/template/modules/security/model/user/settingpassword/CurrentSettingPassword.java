e407d9136104ce68262ed8419c2700e958b02aaa
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-19 20:30:39 +0900

    #71 用語一覧を表示する

--- server-business/src/main/java/template/modules/security/model/user/settingpassword/CurrentSettingPassword.java
+++ server-business/src/main/java/template/modules/security/model/user/settingpassword/CurrentSettingPassword.java
@@ -3,20 +3,25 @@ package template.modules.security.model.user.settingpassword;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.seasar.doma.Column;
 import org.seasar.doma.Entity;
-import org.seasar.doma.Id;
 import org.seasar.doma.Table;
+import template.shared.entity.WhenOccurred;
 import template.shared.entity.id.SnowflakeId;
 
 @Getter
 @Entity(immutable = true)
 @Table(schema = "security", name = "current_setting_password")
 @AllArgsConstructor
-@ToString
+@ToString(exclude = {"encodedPassword"})
 public class CurrentSettingPassword {
-    @Id
+    @Column(name = "user_id")
     private final SnowflakeId userId;
 
+    @Column(name = "latest_when_occurred")
+    private final WhenOccurred whenOccurred;
+
+    @Column(name = "password")
     private final EncodedPassword encodedPassword;
 
 }
