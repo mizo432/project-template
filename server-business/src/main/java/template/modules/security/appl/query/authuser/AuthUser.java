d843e760f5b179d2837b9fcb9a35bdec6a85fe7a
k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2023-09-20 13:42:07 +0900

    #71 用語一覧を表示する

--- server-business/src/main/java/template/modules/security/appl/query/authuser/AuthUser.java
+++ server-business/src/main/java/template/modules/security/appl/query/authuser/AuthUser.java
@@ -1,24 +1,41 @@
 package template.modules.security.appl.query.authuser;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import template.modules.security.model.user.User;
 import template.modules.security.model.user.settingpassword.EncodedPassword;
 import template.shared.value.MultiValue;
 
+/**
+ * 認証ユーザー
+ */
 @AllArgsConstructor
 @Getter
 @ToString(exclude = {"encodedPassword"})
+@EqualsAndHashCode
 public class AuthUser implements MultiValue<AuthUser> {
 
     private final User user;
     private final EncodedPassword encodedPassword;
 
+    /**
+     * 再生成
+     *
+     * @param user            ユーザ
+     * @param encodedPassword エンコード済みパスワード
+     * @return 認証ユーザ
+     */
     public static AuthUser reconstruct(final User user, final EncodedPassword encodedPassword) {
         return new AuthUser(user, encodedPassword);
     }
 
+    /**
+     * 表法が空かを返却する
+     *
+     * @return 空の場合true
+     */
     public boolean isEmpty() {
         return user.isEmpty() && encodedPassword.isEmpty();
     }
