e6bb100fb9d9d0ba9abd298f1b97ea21a450d36e
k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2023-09-20 10:39:13 +0900

    #71 用語一覧を表示する

--- server-business/src/main/java/template/modules/security/model/user/assignmentRole/AssignmentRole.java
+++ server-business/src/main/java/template/modules/security/model/user/assignmentRole/AssignmentRole.java
@@ -21,6 +21,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
+/**
+ * ロール割当
+ */
 @Getter
 @Entity(immutable = true)
 @Table(schema = "security", name = "assignment_role")
@@ -38,18 +41,40 @@ public class AssignmentRole {
     @Column(name = "when_occurred")
     private final WhenOccurred whenOccurred;
 
-    public static AssignmentRole create(SnowflakeId userId, SnowflakeId roleId) {
+    /**
+     * ファクトリーメソッド
+     *
+     * @param userId ユーザID
+     * @param roleId ロールID
+     * @return ロール割当
+     */
+    public static AssignmentRole create(final SnowflakeId userId, final SnowflakeId roleId) {
         return new AssignmentRole(SnowflakeId.newInstance(), userId, roleId, WhenOccurred.current());
 
     }
 
+    /**
+     * ロール割当ファーストクラスコレクション
+     */
     public static class AssignmentsRole extends AbstractListValue<AssignmentRole> {
 
-        public AssignmentsRole(final Collection<AssignmentRole> value) {
+        /**
+         * コンストラクター
+         *
+         * @param value 値
+         */
+        /* default */ AssignmentsRole(final Collection<AssignmentRole> value) {
             super(value);
         }
 
-        public static AssignmentsRole initialize(User.Users users, Role.Roles roles) {
+        /**
+         * 初期データの作成
+         *
+         * @param users ユーザー（複数）
+         * @param roles ロール（複数）
+         * @return ロール割当ファーストクラスコレクション
+         */
+        public static AssignmentsRole initialize(final User.Users users, final Role.Roles roles) {
             List<AssignmentRole> assignmentRoleList = Lists2.newArrayList();
             final Optional<User> adminOptionalUser = users.findByUserCode(UserCode.of("admin"));
             User adminUser = adminOptionalUser.orElseThrow(RuntimeException::new);
@@ -66,16 +91,32 @@ public class AssignmentRole {
             return of(assignmentRoleList);
         }
 
+        /**
+         * ファクトリーメソッド
+         *
+         * @param value 値
+         * @return ロール割当ファーストクラスコレクション
+         */
         private static AssignmentsRole of(final Collection<AssignmentRole> value) {
             return new AssignmentsRole(value);
 
         }
 
+        /**
+         * 文字列表現
+         *
+         * @return 文字列
+         */
         @Override
         public String asString() {
             return toString();
         }
 
+        /**
+         * すべての要素に処理を行う
+         *
+         * @param consumer 対象処理
+         */
         public void forEach(final Consumer<AssignmentRole> consumer) {
             value.forEach(consumer);
         }
