4dbb135af8b49322c9c1e78359a7f4e7d8b62652
k-mizoguchi <hachirouuemon.mizoguchi@gmail.com>
2023-09-22 14:47:28 +0900

    #74 ログイン画面を独自実装する

--- web-backend/src/main/java/template/modules/other/presentation/web/IndexController.java
+++ web-backend/src/main/java/template/modules/other/presentation/web/IndexController.java
@@ -1,15 +1,33 @@
 package template.modules.other.presentation.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = {"/", "/index"})
 public class IndexController {
+
     @GetMapping
     public String get() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login";
+
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm) {
+        return "login";
+
+    }
+
 }
 
---
+++ web-backend/src/main/java/template/modules/other/presentation/web/LoginForm.java
@@ -0,0 +1,9 @@
+package template.modules.other.presentation.web;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    private String username;
+    private String password;
+}
