5712f4ac9cf9f53ae1dab6310a797e286ed0ed6d
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-23 15:37:36 +0900

    #74 ログイン画面を独自実装する

--- web-backend/src/main/java/template/shared/security/UserDetailServiceImpl.java
+++ web-backend/src/main/java/template/shared/security/UserDetailServiceImpl.java
@@ -1,24 +1,33 @@
 package template.shared.security;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import template.modules.security.appl.query.authuser.AuthUser;
-import template.modules.security.appl.query.authuser.AuthUserQuery;
+import template.modules.security.appl.query.user.AuthUser;
+import template.modules.security.appl.query.user.AuthUserQuery;
+import template.modules.security.appl.query.user.role.CurrentAssignmentRole;
+import template.modules.security.appl.query.user.role.CurrentAssignmentRoleQuery;
 import template.modules.security.model.user.UserCode;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private final AuthUserQuery authUserQuery;
+
+    private final CurrentAssignmentRoleQuery currentAssignmentRoleQuery;
 
     @Override
     public SecurityUserDetails loadUserByUsername(final String userCode) throws UsernameNotFoundException {
         final AuthUser authUser = authUserQuery.findByUserCode(UserCode.of(userCode));
         if (authUser.isEmpty())
             throw new UsernameNotFoundException("操作員が見つかりません");
-        return SecurityUserDetails.reconstruct(authUser);
+        CurrentAssignmentRole.AssignmentRoles roles = currentAssignmentRoleQuery.selectByUserId(authUser.userId());
+        SecurityUserDetails user = SecurityUserDetails.reconstruct(authUser, roles);
+        log.debug(user.toString());
+        return user;
     }
 }
