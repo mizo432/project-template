7b2ee8f23cde9afc7f29a322e28608328a97f3ea
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-09-25 19:59:04 +0900

    #71 用語一覧を表示する
    サイドバーを調整した

--- web-backend/src/main/java/template/modules/other/presentation/web/IndexController.java
+++ web-backend/src/main/java/template/modules/other/presentation/web/IndexController.java
@@ -1,10 +1,7 @@
 package template.modules.other.presentation.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,19 +11,6 @@ public class IndexController {
     @GetMapping
     public String get() {
         return "index";
-    }
-
-    @GetMapping("/login")
-    public String showLoginForm(Model model) {
-        model.addAttribute("loginForm", new LoginForm());
-        return "login";
-
-    }
-
-    @PostMapping("/login")
-    public String login(@ModelAttribute LoginForm loginForm) {
-        return "login";
-
     }
 
 }
