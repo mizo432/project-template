e2edc97d6fb096255910894e681a21b1fe31a484
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2023-10-01 08:24:16 +0900

    #82 Jenkinsfileでjunitの結果を収集する

--- common-primitive/src/test/java/com/undecided/primitive/array/Arrays2Test.java
+++ common-primitive/src/test/java/com/undecided/primitive/array/Arrays2Test.java
@@ -1,14 +1,14 @@
 package com.undecided.primitive.array;
 
+import com.undecided.test.size.SmallTest;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 class Arrays2Test {
 
     /**
      * Method under test: {@link Arrays2#isEmpty(Object[])}
      */
-    @Test
+    @SmallTest
     void testIsEmpty() {
         Assertions.assertFalse(Arrays2.isEmpty(new Object[]{"Reference Array"}));
         Assertions.assertTrue(Arrays2.isEmpty(new Object[]{}));
@@ -17,7 +17,7 @@ class Arrays2Test {
     /**
      * Method under test: {@link Arrays2#isEmpty(Object[])}
      */
-    @Test
+    @SmallTest
     void testIsEmpty2() {
         Assertions.assertThrows(NullPointerException.class, () -> Arrays2.isEmpty(null));
 
@@ -26,7 +26,7 @@ class Arrays2Test {
     /**
      * Method under test: {@link Arrays2#isEmptySilently(Object[])}
      */
-    @Test
+    @SmallTest
     void testIsEmptySilently() {
         Assertions.assertFalse(Arrays2.isEmptySilently(new Object[]{"Reference Array"}));
         Assertions.assertTrue(Arrays2.isEmptySilently(new Object[]{}));
